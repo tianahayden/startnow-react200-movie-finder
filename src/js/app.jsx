f5fd054dd1abcb19cc40f0f5f5d89f72ec18a3d2
Tiana Hayden <tiana.hayden@me.com>
2018-10-22 13:28:23 -0700

    routing configured for home page and movie page. movie search and movie detail compoents are rendering

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import { 
+  HashRouter as Router, 
+  Route 
+} from 'react-router-dom';
+
+import MovieDetails from './Components/MovieDetails/MovieDetails'
+import MovieSearch from './Components/MovieSearch/MovieSearch';
 
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello World</h1>
-    );
+      <Router>
+        <div className='container'> 
+          <Route exact path='/' component={ MovieSearch } />
+          <Route path='/movie/:id' component={ MovieDetails } />
+        </div>
+      </Router>
+    )
   }
 }
--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from './reducers';
+import { createStore, applyMiddleware, compose } from 'redux';
+import promiseMiddleware from 'redux-promise-middleware';
+import rootReducer from './rootReducer';
 import App from './app';
 
-/* eslint-disable no-underscore-dangle */
-const store = createStore(
-  reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-/* eslint-enable */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(
+  applyMiddleware(
+    promiseMiddleware()
+  )
+));
 
 
 render(
